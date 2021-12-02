10e1da0ec745fc673f5e3a9ba9be532a1fd02441
mkyu0917 <mkyu0917@gmail.com>
2021-12-02 13:09:24 +0900

    SpringBoot 프로젝트 및 리액트 연동

--- src/main/java/com/spring/react/board/controller/mainController.java
+++ src/main/java/com/spring/react/board/controller/mainController.java
@@ -1,5 +1,6 @@
 package com.spring.react.board.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,10 +8,10 @@ import java.util.Date;
 
 @RestController
 public class mainController {
-
+    @CrossOrigin(origins = "http://localhost:3000") // cors되나..?
     @GetMapping("/api/main")
     public String main(){
-
+                Date dt = new Date();
         return "현재 시각은 " + new Date() + " 입니다.";
 
     }
